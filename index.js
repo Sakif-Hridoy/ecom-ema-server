f5594d69cf8d4ee1ac754217b5252280f700171a
Sakif Hridoy <sakifworkspace@gmail.com>
2021-12-25 23:02:28 +0600

    message

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://emaWatson:emaWatsonPotter81@cluster0.djg6r.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = 6060;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  console.log(err)
+  const products = client.db("amazonStore").collection("items");
+  console.log('database connected')
+  app.post('/addProduct', (req,res)=>{
+    const product = req.body;
+    console.log(product);
+    products.insertOne(product)
+    .then(result=>{
+      console.log(result);
+    })
+  })
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/',(req,res)=>{
+  res.send("Hello Hello Express");
 })
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
